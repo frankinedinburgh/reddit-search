7f9dfeeae7baff7264f6d65dd5752acab90b54eb
frank <frank@pond5.com>
2021-01-31 17:59:13 +0000

    remove unused code

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,6 @@ import App from "./App";
 describe("App", () => {
   test("renders learn react link", () => {
     render(<App />);
-    // console.log()
-    // const linkElement = screen.getByText(/learn react/i);
-    // expect(linkElement).toBeInTheDocument();
-    console.log(screen);
     screen.debug();
   });
 });
